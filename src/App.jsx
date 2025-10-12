da6872aeda2444ba414307281f0bfe6c28eb53b6
Felipe Garcia <luisfelipeglondo@gmail.com>
2025-10-11 21:45:16 -0500

    refactor App: error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-import Login from './pages/Login'
 
 function App() {
   return (
@@ -8,7 +7,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
